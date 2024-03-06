514602f067a297a4a1e4b0a790e4dc81f00d8b74
tomiwadotJS <victorgbayedev@gmail.com>
2024-03-06 04:13:00 +0000

    build changes

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,28 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+  ],
+  build: {
+    lib: {
+      entry: 'src/components/index.tsx', // Update this path to your library's entry point
+      name: 'prmisUI', // The global name used when using UMD
+      fileName: (format) => `primis-ui.${format}.js` // Output file name
+    },
+    rollupOptions: {
+      // Externalize peer dependencies
+      external: ['react', 'react-dom'],
+      output: {
+        // Configure globals for UMD build; necessary for externalized imports
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM'
+        }
+      }
+    },
+    // Optionally, specify sourcemap generation for better debugging
+    sourcemap: true,
+  }
+});
